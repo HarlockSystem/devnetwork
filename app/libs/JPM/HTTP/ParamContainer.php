2c07722f2e6d6c6c2e4ebbcbcefbb4a708825a99
linkus <manu@example.com>
2017-09-03 12:44:15 +0200

    add Request componant

---
+++ app/libs/JPM/HTTP/ParamContainer.php
@@ -0,0 +1,81 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace JPM\HTTP;
+
+/**
+ * Description of ParamContainer
+ *
+ * @author linkus
+ */
+class ParamContainer
+{
+    protected $parameters;
+    
+    public function __construct(array $parameters = [])
+    {
+        $this->parameters = $parameters;
+    }
+    
+    /**
+     * Returns a parameter by name.
+     *
+     * @param string $key     The key
+     * @param mixed  $default The default value if the parameter key does not exist
+     *
+     * @return mixed
+     */
+    public function get($key, $default = null)
+    {
+        return array_key_exists($key, $this->parameters) ? $this->parameters[$key] : $default;
+    }
+    
+    /**
+     * Sets a parameter by name.
+     *
+     * @param string $key   The key
+     * @param mixed  $value The value
+     */
+    public function set($key, $value)
+    {
+        $this->parameters[$key] = $value;
+    }
+    
+    /**
+     * Returns true if the parameter is defined.
+     *
+     * @param string $key The key
+     *
+     * @return bool true if the parameter exists, false otherwise
+     */
+    public function has($key)
+    {
+        return array_key_exists($key, $this->parameters);
+    }
+    
+    /**
+     * Removes a parameter.
+     *
+     * @param string $key The key
+     */
+    public function remove($key)
+    {
+        unset($this->parameters[$key]);
+    }
+    
+    /**
+     * Returns the parameters.
+     *
+     * @return array An array of parameters
+     */
+    public function all()
+    {
+        return $this->parameters;
+    }
+    
+}
---
+++ app/libs/JPM/HTTP/Request.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace JPM\HTTP;
+
+use JPM\HTTP\ParamContainer;
+
+/**
+ * Description of Request
+ *
+ * @author linkus
+ */
+class Request
+{
+    /**
+     * Request body parameters ($_POST).
+     *
+     * @var \JPM\HTTP\ParamContainer
+     */
+    public $request;
+
+    /**
+     * Query string parameters ($_GET).
+     *
+     * @var \JPM\HTTP\ParamContainer
+     */
+    public $query;
+
+    /**
+     * Server and execution environment parameters ($_SERVER).
+     *
+     * @var \JPM\HTTP\ParamContainer
+     */
+    public $server;
+
+    /**
+     * Uploaded files ($_FILES).
+     *
+     * @var \JPM\HTTP\ParamContainer
+     */
+    public $files;
+
+    /**
+     * Cookies ($_COOKIE).
+     *
+     * @var \JPM\HTTP\ParamContainer
+     */
+    public $cookies;
+
+    /**
+     * Headers (taken from the $_SERVER).
+     *
+     * @var \JPM\HTTP\ParamContainer
+     */
+    public $headers;
+
+    public function __construct()
+    {
+        $this->initialize();
+    }
+
+    protected function initialize()
+    {
+        $this->request = new ParamContainer($_POST);
+        $this->query = new ParamContainer($_GET);
+        $this->server = new ParamContainer($_SERVER);
+        $this->files = new ParamContainer($_FILES);
+        $this->cookies = new ParamContainer($_COOKIE);
+        $this->headers = new ParamContainer();
+        foreach ($this->server->all() as $key => $value) {
+            if (0 === strpos($key, 'HTTP_')) {
+                $this->headers->set(substr($key, 5), $value);
+                $this->server->remove($key);
+            }
+        }
+        if (!$this->server->has('PATH_INFO')) {
+            $this->server->set('PATH_INFO', '/');
+        }
+    }
+
+}
