d8a60c9e5e752b0c6cdb4a006b55841ac99d1bd8
was137 <was137.strasbourg@3wa.fr>
2017-09-04 09:35:05 +0200

    update db

--- DEV/devnetwork.sql
+++ DEV/devnetwork.sql
@@ -1,81 +1,120 @@
--- phpMyAdmin SQL Dump
--- version 4.7.3
--- https://www.phpmyadmin.net/
---
--- Host: localhost
--- Generation Time: Sep 04, 2017 at 01:31 AM
--- Server version: 10.1.26-MariaDB
--- PHP Version: 7.1.8
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
+CREATE SCHEMA IF NOT EXISTS `devnetwork_dev` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
+USE `devnetwork_dev` ;
 
---
--- Database: `devnetwork_dev`
---
+-- -----------------------------------------------------
+-- Table `devnetwork_dev`.`User`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `devnetwork_dev`.`User` (
+  `id` INT NOT NULL,
+  `nick` VARCHAR(65) NOT NULL COMMENT 'pseudo',
+  `email` VARCHAR(100) NOT NULL,
+  `password` VARCHAR(60) NULL,
+  `firstname` VARCHAR(65) NULL,
+  `lastname` VARCHAR(65) NULL,
+  `skill` VARCHAR(1000) NULL COMMENT 'user skill',
+  `bio` VARCHAR(1000) NULL,
+  `jobStatus` INT(11) NULL,
+  `createdAt` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  `updatedAt` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  `settings` VARCHAR(1000) NULL COMMENT 'profile display',
+  `img` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `nick_UNIQUE` (`nick` ASC),
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC))
+ENGINE = InnoDB;
 
--- --------------------------------------------------------
 
---
--- Table structure for table `Post`
---
+-- -----------------------------------------------------
+-- Table `devnetwork_dev`.`Post`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `devnetwork_dev`.`Post` (
+  `id` INT NOT NULL,
+  `title` VARCHAR(45) NOT NULL,
+  `contentType` TINYTEXT NOT NULL,
+  `content` VARCHAR(1000) NOT NULL,
+  `createdAt` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  `updatedAt` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  `User_id` INT NOT NULL,
+  PRIMARY KEY (`id`, `User_id`),
+  INDEX `fk_Post_User_idx` (`User_id` ASC),
+  CONSTRAINT `fk_Post_User`
+    FOREIGN KEY (`User_id`)
+    REFERENCES `devnetwork_dev`.`User` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
-CREATE TABLE `Post` (
-  `id` int(11) NOT NULL,
-  `title` varchar(65) NOT NULL,
-  `contentType` tinyint(1) NOT NULL,
-  `content` varchar(1000) NOT NULL,
-  `createdAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updatedAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
+-- -----------------------------------------------------
+-- Table `devnetwork_dev`.`User`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `devnetwork_dev`.`User` (
+  `id` INT NOT NULL,
+  `nick` VARCHAR(65) NOT NULL COMMENT 'pseudo',
+  `email` VARCHAR(100) NOT NULL,
+  `password` VARCHAR(60) NULL,
+  `firstname` VARCHAR(65) NULL,
+  `lastname` VARCHAR(65) NULL,
+  `skill` VARCHAR(1000) NULL COMMENT 'user skill',
+  `bio` VARCHAR(1000) NULL,
+  `jobStatus` INT(11) NULL,
+  `createdAt` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  `updatedAt` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  `settings` VARCHAR(1000) NULL COMMENT 'profile display',
+  `img` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `nick_UNIQUE` (`nick` ASC),
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC))
+ENGINE = InnoDB;
 
---
--- Table structure for table `User`
---
 
-CREATE TABLE `User` (
-  `id` int(11) NOT NULL,
-  `nick` varchar(65) NOT NULL,
-  `email` varchar(191) NOT NULL,
-  `password` varchar(191) NOT NULL,
-  `firstname` varchar(65) DEFAULT NULL,
-  `lastname` varchar(65) DEFAULT NULL,
-  `skill` varchar(1000) DEFAULT NULL,
-  `bio` varchar(1000) DEFAULT NULL,
-  `jobStatus` int(11) DEFAULT NULL,
-  `createdAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updatedAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `settings` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+-- -----------------------------------------------------
+-- Table `devnetwork_dev`.`Post`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `devnetwork_dev`.`Post` (
+  `id` INT NOT NULL,
+  `title` VARCHAR(45) NOT NULL,
+  `contentType` TINYTEXT NOT NULL,
+  `content` VARCHAR(1000) NOT NULL,
+  `createdAt` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  `updatedAt` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+  `User_id` INT NOT NULL,
+  PRIMARY KEY (`id`, `User_id`),
+  INDEX `fk_Post_User_idx` (`User_id` ASC),
+  CONSTRAINT `fk_Post_User`
+    FOREIGN KEY (`User_id`)
+    REFERENCES `devnetwork_dev`.`User` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
---
--- Indexes for dumped tables
---
 
---
--- Indexes for table `Post`
---
-ALTER TABLE `Post`
-  ADD PRIMARY KEY (`id`);
+-- -----------------------------------------------------
+-- Table `devnetwork_dev`.`FavoriteUserPost`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `devnetwork_dev`.`FavoriteUserPost` (
+  `User_id` INT NOT NULL,
+  `Post_id` INT NOT NULL,
+  PRIMARY KEY (`User_id`, `Post_id`),
+  INDEX `fk_User_has_Post_Post1_idx` (`Post_id` ASC),
+  INDEX `fk_User_has_Post_User1_idx` (`User_id` ASC),
+  CONSTRAINT `fk_User_has_Post_User1`
+    FOREIGN KEY (`User_id`)
+    REFERENCES `devnetwork_dev`.`User` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_User_has_Post_Post1`
+    FOREIGN KEY (`Post_id`)
+    REFERENCES `devnetwork_dev`.`Post` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
---
--- Indexes for table `User`
---
-ALTER TABLE `User`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`),
-  ADD UNIQUE KEY `nick` (`nick`);
 
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `User`
---
-ALTER TABLE `User`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;COMMIT;
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
