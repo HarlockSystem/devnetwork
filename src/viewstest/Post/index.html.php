84e83ae5ed640a6ec7c796a83cabd045d58c2d0c
was137 <was137.strasbourg@3wa.fr>
2017-09-05 14:59:54 +0200

    ajout des vues Posts

---
+++ src/viewstest/Post/index.html.php
@@ -0,0 +1,3 @@
+<?php $this->layout('layout', ['title' => 'Liste des Posts ']) ?>
+
+<?php var_dump($user) ?>
---
+++ src/viewstest/Post/show.html.php
@@ -0,0 +1,3 @@
+<?php $this->layout('layout', ['title' => 'Post']) ?>
+
+<?php // var_dump($post) ?>
